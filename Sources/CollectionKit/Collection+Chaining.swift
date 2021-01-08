48e7170e9bcca842f31546ca18bc8ee26fad0bfd
GeneralD <yumejustice@gmail.com>
2021-01-08 14:46:16 +0900

    add a document

---
+++ Sources/CollectionKit/Collection+Chaining.swift
@@ -0,0 +1,54 @@
+//
+//  Collection+Chaining.swift
+//  CollectionKit
+//
+//  Created by Yumenosuke Koukata on 2020/12/29.
+//  Copyright © 2019 ZYXW. All rights reserved.
+//
+
+/// You can get this middle object via array subscript without argument.
+/// By using this object, array can seems to have a new syntax, *array-chaining*.
+///
+/// Example:
+/// ```
+/// struct Person {
+///     let name: String
+///     let age: Int
+///     let friends: [Person]
+/// }
+///
+/// let people: [Person] = [
+///     .init(name: "me", age: 32, friends: [
+///         .init(name: "Tom", age: 40, friends: [])
+///     ]),
+///     .init(name: "you", age: 28, friends: [
+///         .init(name: "Nancy", age: 10, friends: []),
+///         .init(name: "Freddie", age: 20, friends: [
+///             .init(name: "Bob", age: 50, friends: [])
+///         ])
+///     ])
+/// ]
+///
+/// let ourFriends = people[].friends[].name // ["Tom", "Nancy", "Freddie"]
+/// let sumAge = people[].age.reduce(0, +) // 60
+/// ```
+@dynamicMemberLookup
+public struct ArrayMemberLookupReference<C: Collection> {
+	fileprivate let source: C
+	
+	subscript<Property>(dynamicMember keyPath: KeyPath<C.Element, Property>) -> [Property] {
+		source.map { t in t[keyPath: keyPath] }
+	}
+}
+
+public extension Collection {
+	subscript() -> ArrayMemberLookupReference<Self> {
+		.init(source: self)
+	}
+}
+
+public extension Collection where Element: Collection {
+	subscript() -> ArrayMemberLookupReference<[Element.Element]> {
+		.init(source: lazy.flatten)
+	}
+}
--- Tests/CollectionKitTests/Collection+ChainingTest.swift
+++ Tests/CollectionKitTests/Collection+ChainingTest.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import CollectionKit
 
-class CollectionMethodChainingTest: XCTestCase {
+class CollectionChainingTest: XCTestCase {
 	
 	private struct Person {
 		let name: String
@@ -17,14 +17,22 @@ class CollectionMethodChainingTest: XCTestCase {
 		let friends: [Person]
 	}
 	
-	private let people: [Person] = [.init(name: "me", age: 32, friends: [.init(name: "Tom", age: 40, friends: [])]), .init(name: "you", age: 28, friends: [.init(name: "Nancy", age: 10, friends: []), .init(name: "Freddie", age: 20, friends: [.init(name: "Bob", age: 50, friends: [])])])]
+	private let people: [Person] = [
+		.init(name: "me", age: 32, friends: [
+			.init(name: "Tom", age: 40, friends: [])
+		]),
+		.init(name: "you", age: 28, friends: [
+			.init(name: "Nancy", age: 10, friends: []),
+			.init(name: "Freddie", age: 20, friends: [
+				.init(name: "Bob", age: 50, friends: [])
+			])
+		])
+	]
 	
 	func testChaining() {
-		let totalAge = people[].age
-		XCTAssertEqual(totalAge, [32, 28])
+		XCTAssertEqual(people[].age, [32, 28])
 		
-		let ourFriends = people[].friends[].name
-		XCTAssertEqual(ourFriends, ["Tom", "Nancy", "Freddie"])
+		XCTAssertEqual(people[].friends[].name, ["Tom", "Nancy", "Freddie"])
 		
 		let bob = people[].friends[safe: 1]?[].friends[safe: 1]?[].name.first
 		XCTAssertEqual(bob, "Bob")
