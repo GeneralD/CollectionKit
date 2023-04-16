e1ae3fef8fd3be72c309fd20d7e56e9db30827db
GeneralD <yumejustice@gmail.com>
2023-04-17 01:57:29 +0900

    feat: add new extension functions to Collection+Accumlate

--- Sources/CollectionKit/Collection+Accumlate.swift
+++ Sources/CollectionKit/Collection+Accumlate.swift
@@ -28,11 +28,11 @@ public extension Collection {
 		_ initS: S, _ accumS: (_ accum: S, _ element: Element) throws -> S,
 		_ initT: T, _ accumT: (_ accum: T, _ element: Element) throws -> T,
 		_ result: (_ s: S, _ t: T) throws -> R) rethrows -> R {
-		let t = try reduce((initS, initT)) { accum, elem in
-			(try accumS(accum.0, elem), try accumT(accum.1, elem))
+			let t = try reduce((initS, initT)) { accum, elem in
+				(try accumS(accum.0, elem), try accumT(accum.1, elem))
+			}
+			return try result(t.0, t.1)
 		}
-		return try result(t.0, t.1)
-	}
 	
 	/// Evaluates the 3 results of combining the elements of the sequence using the
 	/// given closures. Finally, the results are combined as one using the closure.
@@ -60,11 +60,11 @@ public extension Collection {
 		_ initT: T, _ accumT: (_ accum: T, _ element: Element) throws -> T,
 		_ initU: U, _ accumU: (_ accum: U, _ element: Element) throws -> U,
 		_ result: (_ s: S, _ t: T, _ u: U) throws -> R) rethrows -> R {
-		let t = try reduce((initS, initT, initU)) { accum, elem in
-			(try accumS(accum.0, elem), try accumT(accum.1, elem), try accumU(accum.2, elem))
+			let t = try reduce((initS, initT, initU)) { accum, elem in
+				(try accumS(accum.0, elem), try accumT(accum.1, elem), try accumU(accum.2, elem))
+			}
+			return try result(t.0, t.1, t.2)
 		}
-		return try result(t.0, t.1, t.2)
-	}
 	
 	/// Evaluates the 4 results of combining the elements of the sequence using the
 	/// given closures. Finally, the results are combined as one using the closure.
@@ -98,11 +98,11 @@ public extension Collection {
 		_ initU: U, _ accumU: (_ accum: U, _ element: Element) throws -> U,
 		_ initV: V, _ accumV: (_ accum: V, _ element: Element) throws -> V,
 		_ result: (_ s: S, _ t: T, _ u: U, _ v: V) throws -> R) rethrows -> R {
-		let t = try reduce((initS, initT, initU, initV)) { accum, elem in
-			(try accumS(accum.0, elem), try accumT(accum.1, elem), try accumU(accum.2, elem), try accumV(accum.3, elem))
+			let t = try reduce((initS, initT, initU, initV)) { accum, elem in
+				(try accumS(accum.0, elem), try accumT(accum.1, elem), try accumU(accum.2, elem), try accumV(accum.3, elem))
+			}
+			return try result(t.0, t.1, t.2, t.3)
 		}
-		return try result(t.0, t.1, t.2, t.3)
-	}
 	
 	/// Evaluates the 5 results of combining the elements of the sequence using the
 	/// given closures. Finally, the results are combined as one using the closure.
@@ -142,11 +142,11 @@ public extension Collection {
 		_ initV: V, _ accumV: (_ accum: V, _ element: Element) throws -> V,
 		_ initW: W, _ accumW: (_ accum: W, _ element: Element) throws -> W,
 		_ result: (_ s: S, _ t: T, _ u: U, _ v: V, _ w: W) throws -> R) rethrows -> R {
-		let t = try reduce((initS, initT, initU, initV, initW)) { accum, elem in
-			(try accumS(accum.0, elem), try accumT(accum.1, elem), try accumU(accum.2, elem), try accumV(accum.3, elem), try accumW(accum.4, elem))
+			let t = try reduce((initS, initT, initU, initV, initW)) { accum, elem in
+				(try accumS(accum.0, elem), try accumT(accum.1, elem), try accumU(accum.2, elem), try accumV(accum.3, elem), try accumW(accum.4, elem))
+			}
+			return try result(t.0, t.1, t.2, t.3, t.4)
 		}
-		return try result(t.0, t.1, t.2, t.3, t.4)
-	}
 	
 	/// Evaluates the 6 results of combining the elements of the sequence using the
 	/// given closures. Finally, the results are combined as one using the closure.
@@ -192,11 +192,11 @@ public extension Collection {
 		_ initW: W, _ accumW: (_ accum: W, _ element: Element) throws -> W,
 		_ initX: X, _ accumX: (_ accum: X, _ element: Element) throws -> X,
 		_ result: (_ s: S, _ t: T, _ u: U, _ v: V, _ w: W, _ x: X) throws -> R) rethrows -> R {
-		let t = try reduce((initS, initT, initU, initV, initW, initX)) { accum, elem in
-			(try accumS(accum.0, elem), try accumT(accum.1, elem), try accumU(accum.2, elem), try accumV(accum.3, elem), try accumW(accum.4, elem), try accumX(accum.5, elem))
+			let t = try reduce((initS, initT, initU, initV, initW, initX)) { accum, elem in
+				(try accumS(accum.0, elem), try accumT(accum.1, elem), try accumU(accum.2, elem), try accumV(accum.3, elem), try accumW(accum.4, elem), try accumX(accum.5, elem))
+			}
+			return try result(t.0, t.1, t.2, t.3, t.4, t.5)
 		}
-		return try result(t.0, t.1, t.2, t.3, t.4, t.5)
-	}
 	
 	/// Evaluates the 7 results of combining the elements of the sequence using the
 	/// given closures. Finally, the results are combined as one using the closure.
@@ -248,11 +248,11 @@ public extension Collection {
 		_ initX: X, _ accumX: (_ accum: X, _ element: Element) throws -> X,
 		_ initY: Y, _ accumY: (_ accum: Y, _ element: Element) throws -> Y,
 		_ result: (_ s: S, _ t: T, _ u: U, _ v: V, _ w: W, _ x: X, _ y: Y) throws -> R) rethrows -> R {
-		let t = try reduce((initS, initT, initU, initV, initW, initX, initY)) { accum, elem in
-			(try accumS(accum.0, elem), try accumT(accum.1, elem), try accumU(accum.2, elem), try accumV(accum.3, elem), try accumW(accum.4, elem), try accumX(accum.5, elem), try accumY(accum.6, elem))
+			let t = try reduce((initS, initT, initU, initV, initW, initX, initY)) { accum, elem in
+				(try accumS(accum.0, elem), try accumT(accum.1, elem), try accumU(accum.2, elem), try accumV(accum.3, elem), try accumW(accum.4, elem), try accumX(accum.5, elem), try accumY(accum.6, elem))
+			}
+			return try result(t.0, t.1, t.2, t.3, t.4, t.5, t.6)
 		}
-		return try result(t.0, t.1, t.2, t.3, t.4, t.5, t.6)
-	}
 	
 	/// Evaluates the 8 results of combining the elements of the sequence using the
 	/// given closures. Finally, the results are combined as one using the closure.
@@ -310,9 +310,142 @@ public extension Collection {
 		_ initY: Y, _ accumY: (_ accum: Y, _ element: Element) throws -> Y,
 		_ initZ: Z, _ accumZ: (_ accum: Z, _ element: Element) throws -> Z,
 		_ result: (_ s: S, _ t: T, _ u: U, _ v: V, _ w: W, _ x: X, _ y: Y, _ z: Z) throws -> R) rethrows -> R {
-		let t = try reduce((initS, initT, initU, initV, initW, initX, initY, initZ)) { accum, elem in
-			(try accumS(accum.0, elem), try accumT(accum.1, elem), try accumU(accum.2, elem), try accumV(accum.3, elem), try accumW(accum.4, elem), try accumX(accum.5, elem), try accumY(accum.6, elem), try accumZ(accum.7, elem))
+			let t = try reduce((initS, initT, initU, initV, initW, initX, initY, initZ)) { accum, elem in
+				(try accumS(accum.0, elem), try accumT(accum.1, elem), try accumU(accum.2, elem), try accumV(accum.3, elem), try accumW(accum.4, elem), try accumX(accum.5, elem), try accumY(accum.6, elem), try accumZ(accum.7, elem))
+			}
+			return try result(t.0, t.1, t.2, t.3, t.4, t.5, t.6, t.7)
 		}
-		return try result(t.0, t.1, t.2, t.3, t.4, t.5, t.6, t.7)
-	}
+
+	/// Returns the results of combining the elements of the sequence using the
+	/// given closure into 2 values.
+	///
+	/// - Parameters:
+	/// 	- into: The values to use as the initial accumulating value.
+	/// 	- accumulate: A closure that updates the accumulating values with an element of the sequence.
+	/// - Returns: The final value.
+	func reduce<S, T>(
+		into s: S,
+		_ t: T,
+		_ accumulate: (_ s: inout S, _ t: inout T, Element) throws -> ()) rethrows -> (S, T) {
+			try reduce(into: (s, t)) { r, elem in
+				try accumulate(&r.0, &r.1, elem)
+			}
+		}
+
+	/// Returns the results of combining the elements of the sequence using the
+	/// given closure into 3 values.
+	///
+	/// - Parameters:
+	/// 	- into: The values to use as the initial accumulating value.
+	/// 	- accumulate: A closure that updates the accumulating values with an element of the sequence.
+	/// - Returns: The final value.
+	func reduce<S, T, U>(
+		into s: S,
+		_ t: T,
+		_ u: U,
+		_ accumulate: (_ s: inout S, _ t: inout T, _ u: inout U, Element) throws -> ()) rethrows -> (S, T, U) {
+			try reduce(into: (s, t, u)) { r, elem in
+				try accumulate(&r.0, &r.1, &r.2, elem)
+			}
+		}
+
+	/// Returns the results of combining the elements of the sequence using the
+	/// given closure into 4 values.
+	///
+	/// - Parameters:
+	/// 	- into: The values to use as the initial accumulating value.
+	/// 	- accumulate: A closure that updates the accumulating values with an element of the sequence.
+	/// - Returns: The final value.
+	func reduce<S, T, U, V>(
+		into s: S,
+		_ t: T,
+		_ u: U,
+		_ v: V,
+		_ accumulate: (_ s: inout S, _ t: inout T, _ u: inout U, _ v: inout V, Element) throws -> ()) rethrows -> (S, T, U, V) {
+			try reduce(into: (s, t, u, v)) { r, elem in
+				try accumulate(&r.0, &r.1, &r.2, &r.3, elem)
+			}
+		}
+
+	/// Returns the results of combining the elements of the sequence using the
+	/// given closure into 5 values.
+	///
+	/// - Parameters:
+	/// 	- into: The values to use as the initial accumulating value.
+	/// 	- accumulate: A closure that updates the accumulating values with an element of the sequence.
+	/// - Returns: The final value.
+	func reduce<S, T, U, V, W>(
+		into s: S,
+		_ t: T,
+		_ u: U,
+		_ v: V,
+		_ w: W,
+		_ accumulate: (_ s: inout S, _ t: inout T, _ u: inout U, _ v: inout V, _ w: inout W, Element) throws -> ()) rethrows -> (S, T, U, V, W) {
+			try reduce(into: (s, t, u, v, w)) { r, elem in
+				try accumulate(&r.0, &r.1, &r.2, &r.3, &r.4, elem)
+			}
+		}
+
+	/// Returns the results of combining the elements of the sequence using the
+	/// given closure into 6 values.
+	///
+	/// - Parameters:
+	/// 	- into: The values to use as the initial accumulating value.
+	/// 	- accumulate: A closure that updates the accumulating values with an element of the sequence.
+	/// - Returns: The final value.
+	func reduce<S, T, U, V, W, X>(
+		into s: S,
+		_ t: T,
+		_ u: U,
+		_ v: V,
+		_ w: W,
+		_ x: X,
+		_ accumulate: (_ s: inout S, _ t: inout T, _ u: inout U, _ v: inout V, _ w: inout W, _ x: inout X, Element) throws -> ()) rethrows -> (S, T, U, V, W, X) {
+			try reduce(into: (s, t, u, v, w, x)) { r, elem in
+				try accumulate(&r.0, &r.1, &r.2, &r.3, &r.4, &r.5, elem)
+			}
+		}
+
+	/// Returns the results of combining the elements of the sequence using the
+	/// given closure into 7 values.
+	///
+	/// - Parameters:
+	/// 	- into: The values to use as the initial accumulating value.
+	/// 	- accumulate: A closure that updates the accumulating values with an element of the sequence.
+	/// - Returns: The final value.
+	func reduce<S, T, U, V, W, X, Y>(
+		into s: S,
+		_ t: T,
+		_ u: U,
+		_ v: V,
+		_ w: W,
+		_ x: X,
+		_ y: Y,
+		_ accumulate: (_ s: inout S, _ t: inout T, _ u: inout U, _ v: inout V, _ w: inout W, _ x: inout X, _ y: inout Y, Element) throws -> ()) rethrows -> (S, T, U, V, W, X, Y) {
+			try reduce(into: (s, t, u, v, w, x, y)) { r, elem in
+				try accumulate(&r.0, &r.1, &r.2, &r.3, &r.4, &r.5, &r.6, elem)
+			}
+		}
+
+	/// Returns the results of combining the elements of the sequence using the
+	/// given closure into 8 values.
+	///
+	/// - Parameters:
+	/// 	- into: The values to use as the initial accumulating value.
+	/// 	- accumulate: A closure that updates the accumulating values with an element of the sequence.
+	/// - Returns: The final value.
+	func reduce<S, T, U, V, W, X, Y, Z>(
+		into s: S,
+		_ t: T,
+		_ u: U,
+		_ v: V,
+		_ w: W,
+		_ x: X,
+		_ y: Y,
+		_ z: Z,
+		_ accumulate: (_ s: inout S, _ t: inout T, _ u: inout U, _ v: inout V, _ w: inout W, _ x: inout X, _ y: inout Y, _ z: inout Z, Element) throws -> ()) rethrows -> (S, T, U, V, W, X, Y, Z) {
+			try reduce(into: (s, t, u, v, w, x, y, z)) { r, elem in
+				try accumulate(&r.0, &r.1, &r.2, &r.3, &r.4, &r.5, &r.6, &r.7, elem)
+			}
+		}
 }
--- Tests/CollectionKitTests/Collection+AccumlateTests.swift
+++ Tests/CollectionKitTests/Collection+AccumlateTests.swift
@@ -21,4 +21,112 @@ class CollectionAccumlateTests: XCTestCase {
 		let mult: (Int, Int) -> Int = (*) // To avoid: `The compiler is unable to type-check this expression in reasonable`
 		XCTAssertEqual((1...4).reduce(1, mult, 100, mult, 10000, mult) { $0 + $1 + $2 }, 242424)
 	}
+
+	func testReduceInto2() {
+		let tuple = (1...10).reduce(into: 0, 1) { s, t, elem in
+			s += elem
+			t *= elem
+		}
+		XCTAssertEqual(tuple.0, 55)
+		XCTAssertEqual(tuple.1, 3628800)
+	}
+
+	func testReduceInto3() {
+		let tuple = (1...10).reduce(into: 0, 1, "") { s, t, u, elem in
+			s += elem
+			t *= elem
+			u += elem.description
+		}
+		XCTAssertEqual(tuple.0, 55)
+		XCTAssertEqual(tuple.1, 3628800)
+		XCTAssertEqual(tuple.2, "12345678910")
+	}
+
+	func testReduceInto4() {
+		let tuple = (1...10).reduce(into: 0, 1, "", 1) { s, t, u, v, elem in
+			s += elem
+			t *= elem
+			u += elem.description
+			v <<= 1
+		}
+		XCTAssertEqual(tuple.0, 55)
+		XCTAssertEqual(tuple.1, 3628800)
+		XCTAssertEqual(tuple.2, "12345678910")
+		XCTAssertEqual(tuple.3, 1024)
+	}
+
+	func testReduceInto5() {
+		let tuple = (1...10).reduce(into: 0, 1, "", 1, 2) { s, t, u, v, w, elem in
+			s += elem
+			t *= elem
+			u += elem.description
+			v <<= 1
+			w <<= 1
+		}
+		XCTAssertEqual(tuple.0, 55)
+		XCTAssertEqual(tuple.1, 3628800)
+		XCTAssertEqual(tuple.2, "12345678910")
+		XCTAssertEqual(tuple.3, 1024)
+		XCTAssertEqual(tuple.4, 2048)
+	}
+
+	func testReduceInto6() {
+		let tuple = (1...10).reduce(into: 0, 1, "", 1, 2, 1.0) { s, t, u, v, w, x, elem in
+			s += elem
+			t *= elem
+			u += elem.description
+			v <<= 1
+			w <<= 1
+			x /= 2
+		}
+		XCTAssertEqual(tuple.0, 55)
+		XCTAssertEqual(tuple.1, 3628800)
+		XCTAssertEqual(tuple.2, "12345678910")
+		XCTAssertEqual(tuple.3, 1024)
+		XCTAssertEqual(tuple.4, 2048)
+		XCTAssertEqual(tuple.5, 0.0009765625)
+	}
+
+	func testReduceInto7() {
+		let tuple = (1...10).reduce(into: 0, 1, "", 1, 2, 1.0, "") { s, t, u, v, w, x, y, elem in
+			s += elem
+			t *= elem
+			u += elem.description
+			v <<= 1
+			w <<= 1
+			x /= 2
+			y = [elem.description, y].unless(\.isEmpty).joined(separator: " ")
+		}
+		XCTAssertEqual(tuple.0, 55)
+		XCTAssertEqual(tuple.1, 3628800)
+		XCTAssertEqual(tuple.2, "12345678910")
+		XCTAssertEqual(tuple.3, 1024)
+		XCTAssertEqual(tuple.4, 2048)
+		XCTAssertEqual(tuple.5, 0.0009765625)
+		XCTAssertEqual(tuple.6, "10 9 8 7 6 5 4 3 2 1")
+	}
+
+	func testReduceInto8() {
+		let tuple = (1...10).reduce(into: 0, 1, "", 1, 2, 1.0, "", "") { s, t, u, v, w, x, y, z, elem in
+			s += elem
+			t *= elem
+			u += elem.description
+			v <<= 1
+			w <<= 1
+			x /= 2
+			if elem.isMultiple(of: 2) {
+				y += elem.description
+			} else {
+				z += elem.description
+			}
+		}
+		XCTAssertEqual(tuple.0, 55)
+		XCTAssertEqual(tuple.1, 3628800)
+		XCTAssertEqual(tuple.2, "12345678910")
+		XCTAssertEqual(tuple.3, 1024)
+		XCTAssertEqual(tuple.4, 2048)
+		XCTAssertEqual(tuple.5, 0.0009765625)
+		XCTAssertEqual(tuple.6, "246810")
+		XCTAssertEqual(tuple.7, "13579")
+	}
 }
